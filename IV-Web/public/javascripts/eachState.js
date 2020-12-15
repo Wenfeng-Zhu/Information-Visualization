b07a176efa055a10fdc3a27f7323f3be05a0c82e
Wenfeng Zhu <wenfeng.zhu@campus.lmu.de>
2020-12-15 16:09:40 +0100

    implement the calendar view and data connection

--- IV-Web/public/javascripts/eachState.js
+++ IV-Web/public/javascripts/eachState.js
@@ -1,163 +1,172 @@
+// let promise = fetch('http://localhost:3000/data/total-cases-by-state');
 
-var calenderChart = echarts.init(document.getElementById('eachState'));
-function getVirtulData(year) {
-    year = year || '2020';
-    var date = +echarts.number.parseDate(year + '-01-01');
-    var end = +echarts.number.parseDate((+year + 1) + '-01-01');
+function getVirtulData(name) {
+    var calenderChart = echarts.init(document.getElementById('eachState'));
+    var date = +echarts.number.parseDate(2020 + '-03-02');
+    var end = +echarts.number.parseDate(2020 + '-12-01');
     var dayTime = 3600 * 24 * 1000;
     var data = [];
-    for (var time = date; time < end; time += dayTime) {
-        data.push([
-            echarts.format.formatTime('yyyy-MM-dd', time),
-            Math.floor(Math.random() * 10000)
-        ]);
-    }
-    return data;
+    fetch('http://localhost:3000/data/infections-by-state-daily').then(jsonResponse => {
+        jsonResponse.json().then(result=>{
+            let i = 0;
+            for (var time = date; time < end; time += dayTime) {
+                data.push([
+                    echarts.format.formatTime('yyyy-MM-dd', time),
+                    result.dailyState[i][name.toString()]
+                ]);
+                i++;
+            }
+            option = {
+                backgroundColor: '#404a59',
+
+                title: {
+                    top: 30,
+                    text: 'Confirmed status in each state',
+                    left: 'center',
+                    textStyle: {
+                        color: '#fff'
+                    }
+                },
+                tooltip: {
+                    trigger: 'item'
+                },
+                legend: {
+                    top: '30',
+                    left: '100',
+                    data: ['confirm', 'Top 12'],
+                    textStyle: {
+                        color: '#fff'
+                    }
+                },
+                calendar: [{
+                    top: '15%',
+                    left: 'center',
+                    range: ['2020-03-02', '2020-06-30'],
+                    splitLine: {
+                        show: true,
+                        lineStyle: {
+                            color: '#000',
+                            width: 4,
+                            type: 'solid'
+                        }
+                    },
+                    yearLabel: {
+                        formatter: '{start}  1st',
+                        textStyle: {
+                            color: '#fff'
+                        }
+                    },
+                    itemStyle: {
+                        color: '#323c48',
+                        borderWidth: 1,
+                        borderColor: '#111'
+                    },
+                    width: '70%',
+                    height: '35%'
+                }, {
+                    top: '60%',
+                    left: 'center',
+                    range: ['2020-07-01', '2020-11-30'],
+                    splitLine: {
+                        show: true,
+                        lineStyle: {
+                            color: '#000',
+                            width: 4,
+                            type: 'solid'
+                        }
+                    },
+                    yearLabel: {
+                        formatter: '{start}  2nd',
+                        textStyle: {
+                            color: '#fff'
+                        }
+                    },
+                    itemStyle: {
+                        color: '#323c48',
+                        borderWidth: 1,
+                        borderColor: '#111'
+                    },
+                    width: '80%',
+                    height: '35%'
+                }],
+                series: [
+                    {
+                        name: 'confirm',
+                        type: 'scatter',
+                        coordinateSystem: 'calendar',
+                        data: data,
+                        symbolSize: function (val) {
+                            return val[1] / 200;
+                        },
+                        itemStyle: {
+                            color: '#ddb926'
+                        }
+                    },
+                    {
+                        name: 'confirm',
+                        type: 'scatter',
+                        coordinateSystem: 'calendar',
+                        calendarIndex: 1,
+                        data: data,
+                        symbolSize: function (val) {
+                            return val[1] / 200;
+                        },
+                        itemStyle: {
+                            color: '#ddb926'
+                        }
+                    },
+                    {
+                        name: 'Top 12',
+                        type: 'effectScatter',
+                        coordinateSystem: 'calendar',
+                        calendarIndex: 1,
+                        data: data.sort(function (a, b) {
+                            return b[1] - a[1];
+                        }).slice(0, 12),
+                        symbolSize: function (val) {
+                            return val[1] / 200;
+                        },
+                        showEffectOn: 'render',
+                        rippleEffect: {
+                            brushType: 'stroke'
+                        },
+                        hoverAnimation: true,
+                        itemStyle: {
+                            color: '#f4e925',
+                            shadowBlur: 10,
+                            shadowColor: '#333'
+                        },
+                        zlevel: 1
+                    },
+                    {
+                        name: 'Top 12',
+                        type: 'effectScatter',
+                        coordinateSystem: 'calendar',
+                        data: data.sort(function (a, b) {
+                            return b[1] - a[1];
+                        }).slice(0, 12),
+                        symbolSize: function (val) {
+                            return val[1] / 200;
+                        },
+                        showEffectOn: 'render',
+                        rippleEffect: {
+                            brushType: 'stroke'
+                        },
+                        hoverAnimation: true,
+                        itemStyle: {
+                            color: '#f4e925',
+                            shadowBlur: 10,
+                            shadowColor: '#333'
+                        },
+                        zlevel: 1
+                    }
+                ]
+            };
+            calenderChart.setOption(option);
+
+        })
+    });
 }
 
 
-var data = getVirtulData(2020);
 
-option = {
-    backgroundColor: '#404a59',
-
-    title: {
-        top: 30,
-        text: 'Confirmed status in each state',
-        left: 'center',
-        textStyle: {
-            color: '#fff'
-        }
-    },
-    tooltip: {
-        trigger: 'item'
-    },
-    legend: {
-        top: '30',
-        left: '100',
-        data: ['confirm', 'Top 12'],
-        textStyle: {
-            color: '#fff'
-        }
-    },
-    calendar: [{
-        top: 100,
-        left: 'center',
-        range: ['2020-01-01', '2020-06-30'],
-        splitLine: {
-            show: true,
-            lineStyle: {
-                color: '#000',
-                width: 4,
-                type: 'solid'
-            }
-        },
-        yearLabel: {
-            formatter: '{start}  1st',
-            textStyle: {
-                color: '#fff'
-            }
-        },
-        itemStyle: {
-            color: '#323c48',
-            borderWidth: 1,
-            borderColor: '#111'
-        }
-    }, {
-        top: 340,
-        left: 'center',
-        range: ['2020-07-01', '2020-12-31'],
-        splitLine: {
-            show: true,
-            lineStyle: {
-                color: '#000',
-                width: 4,
-                type: 'solid'
-            }
-        },
-        yearLabel: {
-            formatter: '{start}  2nd',
-            textStyle: {
-                color: '#fff'
-            }
-        },
-        itemStyle: {
-            color: '#323c48',
-            borderWidth: 1,
-            borderColor: '#111'
-        }
-    }],
-    series: [
-        {
-            name: 'confirm',
-            type: 'scatter',
-            coordinateSystem: 'calendar',
-            data: data,
-            symbolSize: function (val) {
-                return val[1] / 500;
-            },
-            itemStyle: {
-                color: '#ddb926'
-            }
-        },
-        {
-            name: 'confirm',
-            type: 'scatter',
-            coordinateSystem: 'calendar',
-            calendarIndex: 1,
-            data: data,
-            symbolSize: function (val) {
-                return val[1] / 500;
-            },
-            itemStyle: {
-                color: '#ddb926'
-            }
-        },
-        {
-            name: 'Top 12',
-            type: 'effectScatter',
-            coordinateSystem: 'calendar',
-            calendarIndex: 1,
-            data: data.sort(function (a, b) {
-                return b[1] - a[1];
-            }).slice(0, 12),
-            symbolSize: function (val) {
-                return val[1] / 500;
-            },
-            showEffectOn: 'render',
-            rippleEffect: {
-                brushType: 'stroke'
-            },
-            hoverAnimation: true,
-            itemStyle: {
-                color: '#f4e925',
-                shadowBlur: 10,
-                shadowColor: '#333'
-            },
-            zlevel: 1
-        },
-        {
-            name: 'Top 12',
-            type: 'effectScatter',
-            coordinateSystem: 'calendar',
-            data: data.sort(function (a, b) {
-                return b[1] - a[1];
-            }).slice(0, 12),
-            symbolSize: function (val) {
-                return val[1] / 500;
-            },
-            showEffectOn: 'render',
-            rippleEffect: {
-                brushType: 'stroke'
-            },
-            hoverAnimation: true,
-            itemStyle: {
-                color: '#f4e925',
-                shadowBlur: 10,
-                shadowColor: '#333'
-            },
-            zlevel: 1
-        }
-    ]
-};
-calenderChart.setOption(option);
--- IV-Web/routes/data.js
+++ IV-Web/routes/data.js
@@ -2,10 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 var sumOfState = require('../public/DataSources/total-cases-by-state.json')
+var dailyState = require('../public/DataSources/infections-by-state-daily.json')
 
 /* GET users listing. */
 router.get('/total-cases-by-state', function(req, res, next) {
   res.json({sumOfState});
+});
+
+router.get('/infections-by-state-daily',function (req,res,N){
+  res.json({dailyState});
 });
 
 module.exports = router;
