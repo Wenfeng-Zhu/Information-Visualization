c779a5be9eb81a15bc58ff0c6a89deaaa378cb54
ShaohuaTong <shaohua.tong@stud.hs-emden-leer.de>
2020-12-23 16:05:14 +0100

    highcharts test

--- IV-Web/public/javascripts/pietest.js
+++ IV-Web/public/javascripts/pietest.js
@@ -1,3 +1,52 @@
+var pieexample = {
+	chart: {
+		plotBackgroundColor: null,
+		plotBorderWidth: null,
+		plotShadow: false,
+		type: 'pie'
+	},
+	title: {
+		text: '2018 年浏览器市场份额'
+	},
+	tooltip: {
+		pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+	},
+	plotOptions: {
+		pie: {
+			allowPointSelect: true,
+			cursor: 'pointer',
+			dataLabels: {
+				enabled: false
+			},
+			showInLegend: true
+		}
+	},
+	series: [{
+		name: 'Brands',
+		colorByPoint: true,
+		data: [{
+			name: 'Chrome',
+			y: 61.41,
+			sliced: true,
+			selected: true
+		}, {
+			name: 'Internet Explorer',
+			y: 11.84
+		}, {
+			name: 'Firefox',
+			y: 10.85
+		}, {
+			name: 'Edge',
+			y: 4.67
+		}, {
+			name: 'Safari',
+			y: 4.18
+		}, {
+			name: 'Other',
+			y: 7.05
+		}]
+	}]
+};
 
 fetch('http://localhost:3000/data/testdata')
 .then(function(response) {
@@ -27,11 +76,20 @@ fetch('http://localhost:3000/data/testdata')
                 showInLegend: true
             }
         },
-            series: [{}]
+        series: [{}]
     };
-    var data = JSON.stringify(myJson.series).replace(/\{\"/g, "\{").replace(/\"\:/g,"\:").replace(/\,\"/g,"\,");
+    //var data = JSON.stringify(myJson.series).replace(/\{\"/g, "\{").replace(/\"\:/g,"\:").replace(/\,\"/g,"\,");
     //  data = data.replace(/\:/g,"\:\"").replace(/\,/g,"\"\,").replace(/"\[/g,"\[").replace(/\}\"\,\{/g,"\}\,\{").replace(/y\:\"/g,"y\:").replace(/\"true/g,"true").replace(/true\"/g,"true").replace(/d\"/g,"\/d");
-     options.series[0] = data; 
-    console.log(options.series[0]);
+    options.series = myJson.series; 
+
+    if(pieexample == options) 
+        console.log("fuck");
+    else 
+        console.log("shabi");
+    
+    console.log(pieexample);
+     
+    console.log(options);
+
     Highcharts.chart('container', options);
 });
