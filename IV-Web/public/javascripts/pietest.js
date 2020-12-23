cde75666ec064a9dd555905e7357b4514e67c591
Yukun Chen <971038081@qq.com>
2020-12-23 21:35:22 +0800

    pieupdate

--- IV-Web/public/javascripts/pietest.js
+++ IV-Web/public/javascripts/pietest.js
@@ -1,49 +1,37 @@
-Highcharts.chart('container', {
-	chart: {
-		plotBackgroundColor: null,
-		plotBorderWidth: null,
-		plotShadow: false,
-		type: 'pie'
-	},
-	title: {
-		text: '2018 年浏览器市场份额'
-	},
-	tooltip: {
-		pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-	},
-	plotOptions: {
-		pie: {
-			allowPointSelect: true,
-			cursor: 'pointer',
-			dataLabels: {
-				enabled: false
-			},
-			showInLegend: true
-		}
-	},
-	series: [{
-		name: 'Brands',
-		colorByPoint: true,
-		data: [{
-			name: 'Chrome',
-			y: 61.41,
-			sliced: true,
-			selected: true
-		}, {
-			name: 'Internet Explorer',
-			y: 11.84
-		}, {
-			name: 'Firefox',
-			y: 10.85
-		}, {
-			name: 'Edge',
-			y: 4.67
-		}, {
-			name: 'Safari',
-			y: 4.18
-		}, {
-			name: 'Other',
-			y: 7.05
-		}]
-	}]
+
+fetch('http://localhost:3000/data/testdata')
+.then(function(response) {
+    return response.json();
+})
+.then(function(myJson) {
+    var options = {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: '2018 年浏览器市场份额'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
+            series: [{}]
+    };
+    var data = JSON.stringify(myJson.series).replace(/\{\"/g, "\{").replace(/\"\:/g,"\:").replace(/\,\"/g,"\,");
+    //  data = data.replace(/\:/g,"\:\"").replace(/\,/g,"\"\,").replace(/"\[/g,"\[").replace(/\}\"\,\{/g,"\}\,\{").replace(/y\:\"/g,"y\:").replace(/\"true/g,"true").replace(/true\"/g,"true").replace(/d\"/g,"\/d");
+     options.series[0] = data; 
+    console.log(options.series[0]);
+    Highcharts.chart('container', options);
 });
--- IV-Web/routes/data.js
+++ IV-Web/routes/data.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var sumOfState = require('../public/DataSources/total-cases-by-state.json');
 var dailyState = require('../public/DataSources/infections-by-state-daily.json');
 var deathSum = require('../public/DataSources/deaths-by-state.json');
+var series = require('../public/DataSources/testdata.json');
 
 /* GET users listing. */
 router.get('/total-cases-by-state', function (req, res, next) {
@@ -18,4 +19,8 @@ router.get('/deaths-by-state', function (req, res, N) {
     res.json({deathSum});
 });
 
+router.get('/testdata', function(req, res){
+    res.send(series);
+  });
+  
 module.exports = router;
