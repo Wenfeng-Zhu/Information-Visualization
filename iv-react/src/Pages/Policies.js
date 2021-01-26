baf07464dec124fa3773742c8fe849afb445a5fa
chenke <chenke.xie@stud.hs-hannover.de>
2021-01-26 21:26:54 +0800

    draw policy

--- iv-react/src/Pages/Policies.js
+++ iv-react/src/Pages/Policies.js
@@ -1,10 +1,39 @@
-import React from "react";
-function Policies(){
-    return(
-        <div className='policies'>
-            <h1>Policies</h1>
-        </div>
-    )
+import React, {Component} from "react";
+import './Home.css'
+import EpidemicMap from "../components/EpidemicMap";
+import Charts from '../components/Charts';
+import {HeatMap} from '../components/HeatMap';
+
+class Policies extends Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            focusState: null,
+        }
+    }
+    handleChange(e, target) {
+        this.setState({focusState: target})
+    }
+
+
+
+    render() {
+        return (
+            <div className='policies'>
+                <div className='firstView'>
+                    <EpidemicMap
+                        focusState={this.state.focusState}
+                        onFocusStateChange={this.handleChange}
+                    />
+                    <Charts
+                        focusState = {this.state.focusState}
+                    />
+                </div>
+                <HeatMap/>
+            </div>
+        )
+    }
 }
 
 export default Policies;
---
+++ iv-react/src/components/LineChart.js
@@ -0,0 +1,14 @@
+import React, {Component, useState} from "react";
+import * as d3 from 'd3';
+
+class LineChart extends Component{
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    drawChart();
+
+    
+
+}
